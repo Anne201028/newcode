31e0a48e6a33bb609dde6d1734d60113523e7b09
Anne <justcode27@outlook.com>
2022-07-13 23:54:02 +0800

    0713

---
+++ newcode/nc0630/js/comm.js
@@ -0,0 +1,17 @@
+function mainNavEff(){
+    $('#mainNav ul ul').css({
+        'position':'relative',
+        'top':200,
+        'left':0
+    }).hide()
+
+    $('#mainNav>ul>li').hover(function(){
+        $(this).children('ul').show().css({'opacity':0}).stop()
+               .animate({'top':0,'opacity':1},1000);
+    },function(){
+        $(this).children('ul').stop()
+               .animate({'top':200,'opacity':0},1000,function(){
+                $(this).hide
+               });
+    })
+}
